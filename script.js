fc55ec8d580bb46b7ebe935b9580f9f7fa754df9
huskar666 <148646923+huskar666@users.noreply.github.com>
2024-08-31 19:12:46 +0300

    code

---
+++ script.js
@@ -0,0 +1,41 @@
+let age = 14;
+console.log(age);
+
+let name = "Illia";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Секундочку плюс минуточку";
+console.log(myString);
+
+let myNumber = 5;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+prompt("Write your name");
+alert("Congratulations!!!");
+
+let a = confirm("Do you confirm that?");
+
+if (a) {
+    alert("Дякую за підтвердження!");
+} 
+else {
+    alert("Дію відмінено!");
+}
+
+alert("Danger!`")
+let b = confirm("Ця дія небезпечна, ви це підтверджуєте?");
+
+if (b){
+    alert("Дякую за підтвердження!");
+}
+else {
+    alert("Дію відмінено!");
+}
+
